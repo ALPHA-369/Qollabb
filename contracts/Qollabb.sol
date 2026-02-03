199ec96ea43b32b51a7a80a2d5ab21873b94fa05
ALPHA369 <ucjunior369@gmail.com>
2026-02-03 09:01:33 +0100

    feat:  Qollabb contract to include ERC721 interface and update CollabStatus enum

--- contracts/Qollabb.sol
+++ contracts/Qollabb.sol
@@ -1,7 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Qollabb {
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+interface IERC721 {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function mint(address to, uint256 quantity) external payable;
+}
+
+contract Qollabb is IERC721Receiver {
     // Defining collab logic here
     struct CollabRequest {
         address creator;
@@ -9,50 +22,20 @@ contract Qollabb {
         uint256 creatorShare; // percentage
         uint256 partnerShare; // percentage
         uint256 fundsLocked; // in wei
+        uint256 nftId; // Minted NFT ID
+        address nftContract; // NFT collection address
+        uint256 mintPrice; // price per NFT in wei
+        uint256 nftQuantity; // number of NFTs to mint
         CollabStatus status;
     }
 
     enum CollabStatus {
-        OPEN,
-        ACCEPTED,
-        COMPLETED
+        OPEN, // nft collab request created
+        FUNDED, // partner has locked funds
+        MINTED, //NFT has been minted in Qollabb contract
+        SOLD, //NFT has been sold
+        SETTLED // funds have been distributed
     }
     mapping(uint256 => CollabRequest) public collabs;
     uint256 public collabCount;
-
-    function createCollab(
-        address _partner,
-        uint256 _creatorShare,
-        uint256 _partnerShare
-    ) external returns (uint256) {
-        require(
-            _creatorShare + _partnerShare == 100,
-            "Combines shares must equal 100%"
-        );
-
-        uint256 collabId = collabCount;
-
-        collabs[collabId] = CollabRequest({
-            creator: msg.sender,
-            partner: _partner,
-            creatorShare: _creatorShare,
-            partnerShare: _partnerShare,
-            fundsLocked: 0,
-            status: CollabStatus.OPEN
-        });
-
-        collabCount++;
-        return collabId;
-    }
-
-    function lockedFunds(uint256 _collabId) external payable {
-        CollabRequest storage c = collabs[_collabId];
-
-        require(msg.sender == c.partner, "only partner can lock funds");
-        require(c.status == CollabStatus.OPEN, "Collab not open");
-        require(msg.value > 0, "Send ETH to lock funds");
-
-        c.fundsLocked = msg.value;
-        c.status = CollabStatus.ACCEPTED;
-    }
 }
